31d2092d99d14f94b2f5368793b413c22f65df82
tkdgur83@gmail.com <tkdgur83@gmail.com>
2023-01-25 01:33:29 +0900

    Init

--- work_test.py
+++ work_test.py
@@ -1,24 +1,20 @@
 import shutil
 import os
-folder_path = r"G:\분류\SSNI"
+folder_path = r"I:\부엉이\독수리"
 folder_list = os.listdir(folder_path)
 
-fpath = r"I:\분류\SSNI"
+target_path = r"I:\부엉이"
 
+def file_to_move_trg_path(trg_path, fpath):
+    for x in os.listdir(fpath):
+        trg_file = os.path.join(fpath, x)
+        if os.path.isfile(trg_file):
+            yield trg_file
+        elif os.path.isdir(trg_file):
+            yield from file_to_move_trg_path(trg_path, trg_file)
+        else:
+            return
 
-fpath
-for fold in folder_list:
-    for fname in os.listdir(fpath):
-        if fold.upper() in fname.upper():
-            print(fname)
-            trg_folder = os.path.join(folder_path, fold)
-            src_file = os.path.join(fpath, fname)
-            shutil.move(src_file, trg_folder)
-
-
-for fname in folder_list:
-    src_file = os.path.join(folder_path, fname)
-    print(src_file)
-    shutil.move(src_file, fpath)
-
+for x in list(file_to_move_trg_path(target_path, folder_path)):
+    shutil.move(x, target_path)
 
